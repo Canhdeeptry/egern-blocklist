d5813f6061d5fc8b90a8c4357e3a70d9787df9f3
canhdeeptry <canh5002@gmail.com>
2024-09-05 09:37:53 +0700

    done

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 import yaml
 import os
 
-# URL của file TXT
-TXT_URL = 'https://github.com/bigdargon/hostsVN/blob/master/filters/adservers-all.txt'  # Thay thế với URL của bạn
+# URL raw của file TXT
+TXT_URL = 'https://raw.githubusercontent.com/bigdargon/hostsVN/master/filters/adservers-all.txt'  # Thay thế với URL raw của bạn
 YML_FILE = 'docs/blocklist.yml'
 
 def convert_txt_to_yml():
@@ -15,7 +15,7 @@ def convert_txt_to_yml():
     lines = response.text.splitlines()
 
     # Tạo danh sách các domain_suffix_set
-    domain_suffix_set = [line.strip() for line in lines if line.strip()]
+    domain_suffix_set = [line.strip() for line in lines if line.strip() and not line.startswith('#')]
 
     # Tạo dữ liệu YML theo định dạng yêu cầu
     data = {
@@ -29,7 +29,7 @@ def convert_txt_to_yml():
 
     # Ghi dữ liệu vào file YML trong thư mục docs
     with open(YML_FILE, 'w') as file:
-        yaml.dump(data, file, sort_keys=False)
+        yaml.dump(data, file, sort_keys=False, default_flow_style=False)
 
 if __name__ == '__main__':
     convert_txt_to_yml()
