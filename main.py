69e380cdbcb1daba84157f5ea69ed674e90da05c
canhdeeptry <canh5002@gmail.com>
2024-10-06 10:03:45 +0700

    update4

--- main.py
+++ main.py
@@ -2,32 +2,31 @@ import requests
 import yaml
 import os
 
-TXT_URL = ['https://abpvn.com/filter/abpvn-F2blen.txt',
-           'https://raw.githubusercontent.com/bigdargon/hostsVN/master/filters/adservers-all.txt'
-]  
+TXT_URLS = [
+    'https://abpvn.com/filter/abpvn-F2blen.txt',  
+    'https://raw.githubusercontent.com/bigdargon/hostsVN/master/filters/adservers-all.txt' 
+]
 YML_FILE = 'docs/blocklist.yml'
 
 def convert_txt_to_yml():
-    
-    response = requests.get(TXT_URL)
-    response.raise_for_status()
-
-    
-    lines = response.text.splitlines()
-
     domain_set = []  
     domain_suffix_set = []
 
-    
-    for line in lines:
-        line = line.strip()
-        if line and not line.startswith('!'):  
-            if line.startswith('||'):
-                domain_suffix = line[2:].split('^')[0]  
-                domain_suffix_set.append(domain_suffix)
-            elif line.startswith('||'):
-                pass
-    
+    for url in TXT_URLS:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        lines = response.text.splitlines()
+
+        for line in lines:
+            line = line.strip()
+            if line and not line.startswith('!'):  
+                if line.startswith('||'):
+                    domain_suffix = line[2:].split('^')[0]  
+                    domain_suffix_set.append(domain_suffix)
+                else:
+                    domain_set.append(line)  
+
     data = {
         'no_resolve': True,
         'domain_set': domain_set,
